9f843c8026dd7c940dbb736a3fc125451853efc5
Lachie <lachie_james@hotmail.com.au>
2021-02-25 23:10:22 +1100

    #167: Add first unit test

---
+++ test/unit_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('My group', () {
+    test('My first unit test', () {
+      String a = 'b';
+      expect(a, 'b');
+    });
+  });
+}
