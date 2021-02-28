11e624557d7444a7bfcb6d7fefaa10ce2fda6602
lachlan james <lachlan.james@energyaustralia.com.au>
2021-02-28 15:17:45 +1100

    oops

--- scripts/azure-pipelines.sh
+++ scripts/azure-pipelines.sh
@@ -33,7 +33,7 @@ run_integration_tests_android_smartphone() {
     $ANDROID_HOME/tools/bin/avdmanager create avd -n "AndroidSmartPhone" --device "Pixel" -k "system-images;android-29;default;x86"
 
     echo "Starting Emulator"
-    $ANDROID_HOME/emulator/emulator -avd "pixel" -no-snapshot &
+    $ANDROID_HOME/emulator/emulator -avd "Pixel" -no-snapshot &
     $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
     echo "Emulator started"
 
