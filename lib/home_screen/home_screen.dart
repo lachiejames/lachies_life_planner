3d4c630cec7da89f54a0b54e9cbe81d139954e5e
Lachie <lachie_james@hotmail.com.au>
2021-03-07 04:15:45 +1100

    Fix home page test

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -3,8 +3,6 @@ import 'package:lachies_life_planner/home_screen/widgets/text_icon_button.dart';
 import 'package:lachies_life_planner/shared/size_config.dart';
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     if (screenWidthUnit == null || screenHeightUnit == null) {
@@ -12,78 +10,74 @@ class HomeScreen extends StatelessWidget {
     }
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _ExpandedRow(
+              children: <Widget>[
+                TextIconButton(
+                  onPressed: null,
+                  text: 'Tasks',
+                  icon: Icons.check,
+                ),
+                TextIconButton(
+                  onPressed: null,
+                  text: 'Calendar',
+                  icon: Icons.calendar_today,
+                ),
+              ],
+            ),
+            _ExpandedRow(
+              children: <Widget>[
+                TextIconButton(
+                  onPressed: null,
+                  text: 'Fitness',
+                  icon: Icons.directions_run_outlined,
+                ),
+                TextIconButton(
+                  onPressed: null,
+                  text: 'Homework',
+                  icon: Icons.menu_book_outlined,
+                ),
+              ],
+            ),
+            _ExpandedRow(
+              children: <Widget>[
+                TextIconButton(
+                  onPressed: null,
+                  text: 'Finance',
+                  icon: Icons.attach_money,
+                ),
+                TextIconButton(
+                  onPressed: null,
+                  text: 'Goals',
+                  icon: Icons.lightbulb,
+                ),
+              ],
+            ),
+          ],
         ),
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextIconButton(
-                        onPressed: null,
-                        text: 'Tasks',
-                        icon: Icons.check,
-                      ),
-                    ),
-                    Expanded(
-                      child: TextIconButton(
-                        onPressed: null,
-                        text: 'Calendar',
-                        icon: Icons.calendar_today,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextIconButton(
-                        onPressed: null,
-                        text: 'Fitness',
-                        icon: Icons.directions_run_outlined,
-                      ),
-                    ),
-                    Expanded(
-                      child: TextIconButton(
-                        onPressed: null,
-                        text: 'Homework',
-                        icon: Icons.menu_book_outlined,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextIconButton(
-                        onPressed: null,
-                        text: 'Finance',
-                        icon: Icons.attach_money,
-                      ),
-                    ),
-                    Expanded(
-                      child: TextIconButton(
-                        onPressed: null,
-                        text: 'Goals',
-                        icon: Icons.lightbulb,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
+  }
+}
+
+class _ExpandedRow extends StatelessWidget {
+  final List<Widget> children;
+
+  _ExpandedRow({@required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: children,
+      ),
+    );
   }
 }
