474135aa906b2505500a15697349d28cd14f8b4b
Lachie <lachie_james@hotmail.com.au>
2021-03-08 02:42:55 +1100

    Add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lachies_life_planner/home_screen/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  // Allows Firebase services to be used
+  await Firebase.initializeApp();
+
   runApp(LachiesLifePlannerApp());
 }
 
