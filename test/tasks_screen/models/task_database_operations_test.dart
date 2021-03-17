c0e874c33f45ab79e6b00fcdd13e98db45c94bde
Lachie <lachie_james@hotmail.com.au>
2021-03-17 23:01:26 +1100

    Moar test coverage

--- test/tasks_screen/models/task_database_operations_test.dart
+++ test/tasks_screen/models/task_database_operations_test.dart
@@ -11,49 +11,53 @@ void main() {
   group('Task', () {
     setUp(() {
       setFirestoreInstance(MockFirestoreInstance());
+      addTask(mockTask);
     });
 
-    group('addTask()', () {
-      test('adds task to Firestore', () async {
-        addTask(mockTask);
-
-        DocumentSnapshot dbData = await getTaskCollection().doc(mockTask.id).get();
-        expect(dbData.data(), mockTask.toJson());
-      });
-
-      test('can be used multiple times', () async {
-        for (Task task in mockTaskList) {
-          addTask(task);
-
-          DocumentSnapshot dbData = await getTaskCollection().doc(task.id).get();
-          expect(dbData.data(), task.toJson());
-        }
-      });
+    test('addTask() adds task to Firestore', () async {
+      DocumentSnapshot dbData = await getTaskCollection().doc(mockTask.id).get();
+      expect(dbData.data(), mockTask.toJson());
     });
 
-    group('updateTask()', () {
-      test('updates task in Firestore', () async {
-        addTask(mockTask);
-        updateTask(mockTask.copyWith(name: 'new task name'));
+    test('addTask() can be used multiple times', () async {
+      for (Task task in mockTaskList) {
+        addTask(task);
 
-        DocumentSnapshot dbData = await getTaskCollection().doc(mockTask.id).get();
-        expect(dbData.data(), {
-          'id': '1234567890',
-          'dateCreated': Timestamp(12345, 67890),
-          'name': 'new task name',
-          'isComplete': false,
-        });
+        DocumentSnapshot dbData = await getTaskCollection().doc(task.id).get();
+        expect(dbData.data(), task.toJson());
+      }
+    });
+
+    test('updateTask() updates task in Firestore', () async {
+      updateTask(mockTask.copyWith(name: 'new task name'));
+
+      DocumentSnapshot dbData = await getTaskCollection().doc(mockTask.id).get();
+      expect(dbData.data(), {
+        'id': '1234567890',
+        'dateCreated': Timestamp(12345, 67890),
+        'name': 'new task name',
+        'isComplete': false,
       });
     });
 
-    group('removeTask()', () {
-      test('removes task from Firestore', () async {
-        addTask(mockTask);
-        deleteTask(mockTask);
+    test('deleteTask() removes task from Firestore', () async {
+      deleteTask(mockTask);
 
-        DocumentSnapshot dbData = await getTaskCollection().doc(mockTask.id).get();
+      DocumentSnapshot dbData = await getTaskCollection().doc(mockTask.id).get();
+      expect(dbData.data(), null);
+    });
+
+    test('deleteAllTasks() removes all tasks from Firestore', () async {
+      for (Task task in mockTaskList) {
+        addTask(task);
+      }
+
+      await deleteAllTasks();
+
+      for (Task task in mockTaskList) {
+        DocumentSnapshot dbData = await getTaskCollection().doc(task.id).get();
         expect(dbData.data(), null);
-      });
+      }
     });
   });
 }
