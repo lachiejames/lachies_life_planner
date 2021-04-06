f49152c75be1168b7cf5271fef7f123cbfae64e6
Lachie <lachie_james@hotmail.com.au>
2021-04-07 00:14:01 +1000

    Add more bloc tests

--- test/tasks_screen/bloc/task_bloc_test.dart
+++ test/tasks_screen/bloc/task_bloc_test.dart
@@ -22,13 +22,51 @@ void main() {
 
   group('TaskBloc', () {
     blocTest(
-      'LoadTasksEvent results in TasksLoadedState',
+      'LoadTasksEvent emits [TasksLoadingState, TasksLoadedState]',
       build: () {
         when(mockTasksRepository.getTasksStream()).thenAnswer((_) => Stream.value(mockTaskList));
         return TasksBloc(tasksRepository: mockTasksRepository);
       },
       act: (bloc) => bloc.add(LoadTasksEvent()),
       expect: () => [TasksLoadingState(), TasksLoadedState(mockTaskList)],
+    );
+
+    blocTest(
+      'TasksLoadedEvent emits [TasksLoadedState]',
+      build: () {
+        when(mockTasksRepository.getTasksStream()).thenAnswer((_) => Stream.value(mockTaskList));
+        return TasksBloc(tasksRepository: mockTasksRepository);
+      },
+      act: (bloc) => bloc.add(TasksLoadedEvent(mockTaskList)),
+      expect: () => [TasksLoadedState(mockTaskList)],
+    );
+
+    blocTest(
+      'AddTaskEvent emits no state changes',
+      build: () => TasksBloc(tasksRepository: mockTasksRepository),
+      act: (bloc) => bloc.add(AddTaskEvent(mockTask)),
+      expect: () => [],
+    );
+
+    blocTest(
+      'UpdateTaskEvent emits no state changes',
+      build: () => TasksBloc(tasksRepository: mockTasksRepository),
+      act: (bloc) => bloc.add(UpdateTaskEvent(mockTask)),
+      expect: () => [],
+    );
+
+    blocTest(
+      'DeleteTaskEvent emits no state changes',
+      build: () => TasksBloc(tasksRepository: mockTasksRepository),
+      act: (bloc) => bloc.add(DeleteTaskEvent(mockTask)),
+      expect: () => [],
+    );
+
+    blocTest(
+      'DeleteAllTasksEvent emits no state changes',
+      build: () => TasksBloc(tasksRepository: mockTasksRepository),
+      act: (bloc) => bloc.add(DeleteAllTasksEvent()),
+      expect: () => [],
     );
   });
 }
